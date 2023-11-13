a7987b669f1078e47164e230de6f068748ef371c
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-11-13 14:15:45 +0000

    sarah2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  const admins = ["sarahibarron@hotmail.co.uk"];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <h1>Festival App</h1>
+      {isAuthenticated && (
+        <div>
+          <Profile />
+          <LogoutButton />
+        </div>
+      )}
+      {!isAuthenticated && <LoginButton />}
+
+      <h1>Auth0 Demo</h1>
+      <p>Want to login?</p>
+
+      {admins.includes(user?.email) && <p>can put admin stuff in here</p>}
     </>
   );
 }
