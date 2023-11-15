b9d6906ec62518ea3ee96e10168b8bf71a9314fa
EDGARS VALKOVSKIS <EDATBIZ@GMAIL.COM>
2023-11-15 09:14:13 +0000

    cssedmy

--- client/src/pages/MySchedule.jsx
+++ client/src/pages/MySchedule.jsx
@@ -1,25 +1,28 @@
 export default function MySchedule() {
   return (
     <div>
-      <h1 className="h1schedule">Schedule</h1>
+      <h1 className="h1schedule">My Schedule</h1>
       <div className="scheduleflex">
         <div className="containerOne">
           <ul className="box1">
             <li>TIME:</li>
             <li>ARTIST:</li>
             <li>STAGE:</li>
+            <li>RATING:</li>
           </ul>
 
           <ul className="box2">
             <li>TIME:</li>
             <li>ARTIST:</li>
             <li>STAGE:</li>
+            <li>RATING:</li>
           </ul>
 
           <ul className="box3">
             <li>TIME:</li>
             <li>ARTIST:</li>
             <li>STAGE:</li>
+            <li>RATING:</li>
           </ul>
         </div>
 
@@ -28,20 +31,28 @@ export default function MySchedule() {
             <li>TIME:</li>
             <li>ARTIST:</li>
             <li>STAGE:</li>
+            <li>RATING:</li>
           </ul>
 
           <ul className="box5">
             <li>TIME:</li>
             <li>ARTIST:</li>
             <li>STAGE:</li>
+            <li>RATING:</li>
           </ul>
 
           <ul className="box6">
             <li>TIME:</li>
             <li>ARTIST:</li>
             <li>STAGE:</li>
+            <li>RATING:</li>
           </ul>
         </div>
+      </div>
+      <div className="myschedulebuttons">
+        <button>MY SCHEDULE</button>
+        <button>DELETE ARTIST</button>
+        <button>EDIT RATING</button>
       </div>
     </div>
   );
